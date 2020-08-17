84944bc4b8c5416af7496a6fecd632e1752e1604
Stéffany Caroline <45576846+steffanydev@users.noreply.github.com>
2020-08-17 20:34:23 -0300

    Aula 17/08 - Programação Funcional

---
+++ 02_FuncoesPuras.js
@@ -0,0 +1,11 @@
+var valor1 = 4;
+var valor2 = 6;
+
+function soma(v1, v2){
+  var resultado = valor1 + valor2;
+
+  return resultado;
+}
+
+var retorno = soma(valor1, valor2);
+console.log(retorno);
