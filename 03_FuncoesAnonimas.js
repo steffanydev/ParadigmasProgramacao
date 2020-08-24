1e0c9fc9bc70e34dcdef9900bfb5522e73936fa3
Stéffany Caroline <45576846+steffanydev@users.noreply.github.com>
2020-08-24 19:16:00 -0300

    Aula 17/08 - Programação Funcional

---
+++ 03_FuncoesAnonimas.js
@@ -0,0 +1,20 @@
+// Função nomeada
+function boasVindas(){
+  console.log('Olá, Bem-Vindo!');
+}
+
+boasVindas();
+
+// Função anônima
+var boasVindas = function() {
+  console.log('Olá, Bem-Vindo!');
+}
+
+boasVindas();
+
+// Arrow function
+var boasVindas = () => {
+  console.log('Olá, Bem-Vindo!');
+}
+
+boasVindas();
